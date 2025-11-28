136277c6c00788bb40298f5f719f1414962aaee6
bobeeettt <robertang0566@gmail.com>
2025-11-28 12:00:03 +0700

    commit saja

---
+++ latih1.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>Mencoba Menggunakan PHP </title>
+</head>
+<body>
+    <h1>Belajar PHP</h1>
+        <?php
+            echo 'Halo PHP';
+        ?>
+</body>
+</html>
